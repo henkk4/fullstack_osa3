827124d22ff4e0771978e1a082b3a9ddbb1160e3
Henry Rasinkangas <henry.rasinkangas@tuni.fi>
2021-02-16 21:10:28 +0200

    osan 3 aloitus

---
+++ index.js
@@ -0,0 +1,63 @@
+    const express = require('express')
+    const app = express()
+
+    let persons = [
+      {
+        "name": "Arto Hellas",
+        "number": "040-123456",
+        "id": 1
+      },
+      {
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523",
+        "id": 2
+      },
+      {
+        "name": "Dan Abramov",
+        "number": "12-43-234345",
+        "id": 3
+      },
+      {
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122",
+        "id": 4
+      }
+    ]
+
+    
+    app.get('/', (req, res) => {
+      res.send('<h1>Persons</h1>')
+    })
+    
+    app.get('/api/persons', (req, res) => {
+      res.json(persons)
+    })
+
+    app.get('/api/persons/:id', (request, response) => {
+        const id = Number(request.params.id)
+        const person = persons.find(person => person.id === id)
+        
+        if (person) {
+          response.json(person)
+        } else {
+          response.status(404).end()
+        }
+      })
+
+      app.delete('/api/persons/:id', (request, response) => {
+        const id = Number(request.params.id)
+        persons = persons.filter(person => person.id !== id)
+      
+        response.status(204).end()
+      })  
+
+    app.get('/info', (req, res) => {
+        let info = `<p>phonebook has info for ${persons.length} persons<p>
+                    <p>${new Date()}<p>`
+        res.send(info)
+      })
+    
+    const PORT = 3001
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`)
+    })
